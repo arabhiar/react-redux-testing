64e0ed7e607afe8b24506ca4adcc7215a7a5ab4c
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-04-29 23:03:16 +0530

    Fixed App.test.js

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 
 import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import Root from 'Root';
 
-let wrapped;
-beforeEach(() => {
-  wrapped = shallow(<App />);
+let wrapper;
+afterEach(() => {
+  wrapper.unmount();
 });
 
 it('should show a comment box', () => {
-  expect(wrapped.find(CommentBox).length).toEqual(1);
+  wrapper = mount(
+    <Root>
+      <MemoryRouter initialEntries={['/post']}>
+        <App />
+      </MemoryRouter>
+    </Root>
+  );
+
+  wrapper.find('.sign-in').simulate('click');
+  wrapper.update();
+  wrapper.find('.to-post a').simulate('click', {
+    button: 0,
+  });
+  wrapper.update();
+  expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
-it('should show a comment list', () => {
-  expect(wrapped.find(CommentList).length).toEqual(1);
+it('should ahow a comment list', () => {
+  wrapper = mount(
+    <Root>
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    </Root>
+  );
+  expect(wrapper.find(CommentList).length).toEqual(1);
 });
