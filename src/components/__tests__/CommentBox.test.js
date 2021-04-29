df3ab7128cb55b7c0eb5c74644e8659640341059
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-04-29 23:26:16 +0530

    Finalize CommentBox.test.js

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 
-import CommentBox from 'components/CommentBox';
 import Root from 'Root';
 import App from 'components/App';
 
@@ -23,31 +22,31 @@ beforeEach(() => {
   wrapper.update();
 });
 
-// afterEach(() => {
-//   wrapper.unmount();
-// });
+afterEach(() => {
+  wrapper.unmount();
+});
 
 it('should contain a text-area and two buttons', () => {
   expect(wrapper.find('.comment-box textarea').length).toEqual(1);
   expect(wrapper.find('.comment-box button').length).toEqual(2);
 });
 
-// describe('text area', () => {
-//   beforeEach(() => {
-//     wrapper.find('textarea').simulate('change', {
-//       target: {
-//         value: 'this is a comment',
-//       },
-//     });
-//     wrapper.update();
-//   });
-//   it('should have a text-area where user can type in', () => {
-//     expect(wrapper.find('textarea').prop('value')).toEqual('this is a comment');
-//   });
+describe('text area', () => {
+  beforeEach(() => {
+    wrapper.find('textarea').simulate('change', {
+      target: {
+        value: 'this is a comment',
+      },
+    });
+    wrapper.update();
+  });
+  it('should have a text-area where user can type in', () => {
+    expect(wrapper.find('textarea').prop('value')).toEqual('this is a comment');
+  });
 
-//   it('should contain empty text-area after submit', () => {
-//     wrapper.find('form').simulate('submit');
-//     wrapper.update();
-//     expect(wrapper.find('textarea').prop('value')).toEqual('');
-//   });
-// });
+  it('should contain empty text-area after submit', () => {
+    wrapper.find('form').simulate('submit');
+    wrapper.update();
+    expect(wrapper.find('textarea').prop('value')).toEqual('');
+  });
+});
